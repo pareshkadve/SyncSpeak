530bcf463493f3958347c01fb5376a2c0ac684c1
pareshkadve <2403fb8t4gparesh@viva-technology.org>
2025-04-06 20:57:15 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
-
 import { connectDB } from "./lib/db.js";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
@@ -16,7 +13,10 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
+// 🚀 Increase Payload Size Limit
+app.use(express.json({ limit: "10mb" })); 
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
 app.use(cookieParser());
 app.use(
   cors({
